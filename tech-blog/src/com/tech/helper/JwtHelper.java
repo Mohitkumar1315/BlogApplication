c4a31d0aab277da63cebbba36127b923b2820cac
Mohitkumar1315 <mvkrampur1315@gmail.com>
2024-12-05 15:46:34 +0530

    Implementation of JWT is successfully on show post by user

--- tech-blog/src/com/tech/helper/JwtHelper.java
+++ tech-blog/src/com/tech/helper/JwtHelper.java
@@ -1,7 +1,6 @@
 package com.tech.helper;
 
 import java.util.Date;
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
@@ -14,23 +13,15 @@ public class JwtHelper {
             ? System.getenv("JWT_SECRET_KEY") 
             : "defaultSecretForTestingOnly";
 
-        // Generate JWT Token with Role
-        public static String generateToken(String username, String role) {
-            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-            return JWT.create()
-                    .withSubject(username)
-                    .withClaim("role", role)
-                    .withIssuedAt(new Date())
-                    .withExpiresAt(new Date(System.currentTimeMillis() + 3600000)) // 1-hour expiry
-                    .sign(algorithm);
-        }
-    // Validate Token and Check Role
-    public static boolean validateTokenAndRole(String token, String expectedRole) throws JWTVerificationException {
+    // Generate JWT Token with Role
+    public static String generateToken(String username, String role) {
         Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-        JWTVerifier verifier = JWT.require(algorithm).build();
-        DecodedJWT jwt = verifier.verify(token);
-        String role = jwt.getClaim("role").asString();
-        return expectedRole.equals(role);
+        return JWT.create()
+                .withSubject(username)
+                .withClaim("role", role)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 3600000)) // 1-hour expiry
+                .sign(algorithm);
     }
 
     // Validate and Decode JWT
