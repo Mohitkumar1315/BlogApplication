33fa12dff3989d67eaf0ba66a2574f23d644589c
Mohitkumar1315 <mvkrampur1315@gmail.com>
2025-06-01 15:44:49 +0530

    Done some changes for resolving issue with render and railway db config

--- src/com/tech/helper/ConnectionProvider.java
+++ src/com/tech/helper/ConnectionProvider.java
@@ -5,18 +5,19 @@ import java.sql.DriverManager;
 
 public class ConnectionProvider {
     private static Connection con;
-//
+
     public static Connection getConnection() {
         try {
             if (con == null || con.isClosed()) {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                // Use Railway's MySQL URL, user, and password here
-                String url = "mysql://root:LUdkBQVHTJofYxynCqROfnLOwWnKiYXu@mysql.railway.internal:3306/railway";
+
+                // ✅ Public proxy URL from Railway
+                String url = "jdbc:mysql://tramway.proxy.rlwy.net:19045/railway";
                 String user = "root";
                 String password = "LUdkBQVHTJofYxynCqROfnLOwWnKiYXu";
 
                 con = DriverManager.getConnection(url, user, password);
-                // System.out.println("Connected to Railway DB...");
+                System.out.println("Connected to Railway DB");
             }
         } catch (Exception ex) {
             System.out.println("Database connection failed: " + ex);
@@ -24,6 +25,7 @@ public class ConnectionProvider {
         return con;
     }
 }
+
 //package com.tech.helper;
 //import java.sql.*;
 //public class ConnectionProvider 
